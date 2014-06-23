b540d126bc4a89cd00f42adb391b7899a0b940b1
unknown <Olga_Bielkina@EPUALVIW0591.kyiv.epam.com>
2014-06-23 17:55:00 +0300

    breadcrumbs with parent categories

--- client/app/scripts/controllers/breadcrumbsController.js
+++ client/app/scripts/controllers/breadcrumbsController.js
@@ -1,32 +1,51 @@
 Frenzli.BreadcrumbsController = Ember.Controller.extend({
-  needs: ["application"],
+    needs: ["application"],
 
-  breadContent: function() {
-    var routes = Frenzli.get("Router.router.currentHandlerInfos");
-    var tailRoute = routes;
-    tailRoute = tailRoute.slice(1, tailRoute.length - 1);
-    var segments = $.map(tailRoute, function(route) {
-        var context, name;
-        if(route.isDynamic && route.context) {
-            name = route.context.get("id");
-            context = route.context
-        } else {
-            var dotIndex = route.name.lastIndexOf('.') + 1;
-            name = Ember.String.capitalize(route.name.slice(dotIndex));
+    breadContent: function() {
+        var mapper = Frenzli.get("routeToBeadcrumbMap");
+        var routes = Frenzli.get("Router.router.currentHandlerInfos");
+        var tailRoute = routes;
+        tailRoute = tailRoute.slice(1, tailRoute.length - 1);
+        var segments = $.map(tailRoute, function(route) {
+            var context, name, result = [];
+            if (mapper[route.name]) {
+                if(route.isDynamic && route.context) {
+                    //name = route.context.get("id");
+                    name = mapper[route.name](route);
+                    context = route.context;
+                } else {
+                    var dotIndex = route.name.lastIndexOf('.') + 1;
+                    name = Ember.String.capitalize(route.name.slice(dotIndex));
+                }
+                if (Array.isArray(name)) {
+                    for (var i = 0; i < name.length; i++) {
+                        result[i] = {
+                            name: name[i],
+                            routePath: route.name,
+                            context: context
+                        }
+                    }
+                } else  {
+                    result = {
+                        name: name,
+                        routePath: route.name,
+                        context: context
+                    }
+                }
+                return result;
+            }
+            return null;
+        })
+        return segments;
+    }.property("controllers.application.currentPath"),
+
+    actions: {
+        gotoRoute: function(routeOpt) {
+            if(routeOpt.context) {
+              this.transitionToRoute(routeOpt.routePath, routeOpt.context);
+            } else {
+              this.transitionToRoute(routeOpt.routePath);
+            }
         }
-        return {
-            name: name,
-            routePath: route.name,
-            context: context
-        }
-    })
-    return segments;
-  }.property("controllers.application.currentPath"),
-  gotoRoute: function(routeOpt) {
-    if(routeOpt.context) {
-      this.transitionToRoute(routeOpt.routePath, routeOpt.context);
-    } else {
-      this.transitionToRoute(routeOpt.routePath);
     }
-   }
 });
