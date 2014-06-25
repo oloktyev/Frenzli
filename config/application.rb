c6ccfb83e3e386fe67c7b3d6eb9019d0dffdbbf0
oloktyev <oloktyev@gmail.com>
2014-06-25 22:03:26 +0300

    stash changes

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,13 @@ module Frenzli
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    
+    config.action_dispatch.default_headers = {
+      'X-Frame-Options' => 'SAMEORIGIN',
+      'X-XSS-Protection' => '1; mode=block',
+      'X-Content-Type-Options' => 'nosniff',
+      'Access-Control-Allow-Origin' => 'http://localhost:9000',
+      'Access-Control-Request-Method' => '*'
+    }
   end
 end
